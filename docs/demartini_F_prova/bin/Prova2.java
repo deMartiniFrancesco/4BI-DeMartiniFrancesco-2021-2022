467a7caa26a083d9b26e8a8ae8834f0c4efa0774
deMartiniFrancesco-4BI-2020-2021 <73547745+deMartiniFrancesco@users.noreply.github.com>
2021-03-06 09:28:44 +0100

    Delete docs/demartini_F_prova directory

--- docs/demartini_F_prova/bin/Prova2.java
+++
@@ -1,9 +0,0 @@
-
-class Prova2 {
-
-    public static void main(String[] args) {
-    
-        System.out.println("Hello Word!");
-
-    }
-}
--- docs/demartini_F_prova/doc/README.md
+++
@@ -1,5 +0,0 @@
-# Program name: prova.java
----
-
-## Consegna
-prova
