f502944a1d66a2ced0ca7d97f008cc666e6ee874
francesco <frenkydema@gmail.com>
2021-04-30 18:06:44 +0200

    General commit

--- docs/demartini_F_Main/bin/Main.java
+++ docs/demartini_F_Main/bin/Main.java
@@ -2,10 +2,10 @@ package demartini_F_Main.bin;
 
 class Main {
   public static void main(String[] args) {
-    System.out.print("Start");
+    System.out.println("Start");
 
-    System.out.print("Sequenza comandi");
+    System.out.println("Sequenza comandi");
 
-    System.out.print("End");
+    System.out.println("End");
   }
 }
