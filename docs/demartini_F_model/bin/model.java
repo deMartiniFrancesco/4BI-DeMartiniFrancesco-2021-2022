b8b8759fe97017aef6e90ea7f4b78060f332caff
DeMartiniFrancesco <73547745+deMartiniFrancesco@users.noreply.github.com>
2021-10-26 07:54:44 +0200

    Update model.java

--- docs/demartini_F_model/bin/model.java
+++ docs/demartini_F_model/bin/model.java
@@ -2,7 +2,7 @@ package demartini_F_model.bin;
 
 
 class model{
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         System.out.print("Hello, World");
 
