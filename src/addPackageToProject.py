effc8347671783baaadfa479421721e544254b7a
deMartiniFrancesco <18606@studenti.marconiverona.edu.it>
2021-12-07 08:43:09 +0100

    addPackageToProject v1

--- src/addPackageToProject.py
+++ src/addPackageToProject.py
@@ -2,7 +2,7 @@
 __autor__ = "Francesco"
 __version__ = "0101 2021/11/30"
 
-from genericpath import isdir
+from genericpath import isdir, isfile
 import os
 
 
@@ -13,15 +13,39 @@ os.chdir(percorso)
 
 boold = True
 if __name__ == '__main__':
-    if boold:
-        print("Start")
+	if boold:
+		print("Start")
 
-    projectListDir = os.listdir()
+	projectListDir = os.listdir()
 
-    for dir in projectListDir:
-        if isdir(dir) and dir.startswith("demartini"):
-            print(dir)
+	for dir in projectListDir:
+		if isdir(dir) and dir.startswith("demartini"):
+			package = "package " + dir + ".bin;\n\n"
+			dir += "/bin/"
+			if isdir(dir):
+				for javaFilePath in os.listdir(dir):
+					javaFilePath = dir + javaFilePath
+					if isfile(javaFilePath) and javaFilePath.endswith(".java"):
+						print(javaFilePath)
 
-    
-    if boold:
-        print("End")
+						javaFile = open(javaFilePath, "r",)
+						listLines = javaFile.readlines()
+						javaFile.close()
+
+						if(package not in listLines):
+							listLines.insert(0, package)
+							javaFile = open(javaFilePath, "w",)
+							listLines = javaFile.writelines(listLines)
+							javaFile.close()
+
+						
+						
+
+						
+
+
+
+
+	
+	if boold:
+		print("End")
