ef4e06da48bd277b43f11984d7fdf2289921fb21
deMartiniFrancesco <18606@studenti.marconiverona.edu.it>
2022-01-11 09:30:37 +0100

    BlackJack 0.1

--- out/production/Git_INI/production/Git_INI/demartini_F_CarteDaGioco_Game/doc/README.md
+++ out/production/Git_INI/production/Git_INI/demartini_F_CarteDaGioco_Game/doc/README.md
@@ -8,4 +8,5 @@ Creazione gioco blackJack implementando classi Carta e Mazzo
 
 ### Regole
 
-Nel gioco l'asso può valere 11, o 1, le figure valgono 10, mentre le altre carte da gioco valgono il loro valore nominale
+Nel gioco l'asso può valere 11, o 1, le figure valgono 10, mentre le altre carte da gioco valgono il loro valore
+nominale
--- src/demartini_F_CarteDaGioco_Game/bin/BlackJack.java
+++ src/demartini_F_CarteDaGioco_Game/bin/BlackJack.java
@@ -1,114 +1,42 @@
 package demartini_F_CarteDaGioco_Game.bin;
 
-import javax.swing.*;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import demartini_F_CarteDaGioco_Game.bin.Entity.Player;
+import demartini_F_CarteDaGioco_Game.bin.Models.GameStatus;
+
+import java.util.ArrayList;
 
 /**
  * The type Black jack.
  */
-public class BlackJack {
+public class BlackJack implements GameStatus {
 
+    private GameStatusType status;
+    private final ArrayList<Player> players;
 
-    /**
-     * Number input int.
-     *
-     * @param inputDioalog the input dioalog
-     * @return the int
-     */
-    public static int numberInput(String inputDioalog) {
-        int returnedNumber = -1;
-
-        while (returnedNumber <= 0) {
-            try {
-                returnedNumber = Integer.parseInt(input(inputDioalog));
-                if (returnedNumber <= 0) System.out.println("Inserire numero maggiore di 0");
-            } catch (Exception e) { // or your specific exception
-                System.out.println("inserire numero valido");
-            }
-        }
-        return returnedNumber;
+    public BlackJack() {
+        this.status = GameStatusType.START;
+        players = new ArrayList<>();
     }
 
-    public static String input(String inputDioalog) {
-        String output = JOptionPane.showInputDialog(inputDioalog);
-        output = output.toLowerCase();
-        return output;
+
+    public ArrayList<Player> getPlayers() {
+        return players;
     }
 
-    public static void confirmDialog(String inputDioalog, String title) {
-        JOptionPane.showConfirmDialog(null,
-                inputDioalog, title, JOptionPane.DEFAULT_OPTION);
+
+    @Override
+    public void startGame() {
+        status = GameStatusType.START;
     }
 
-    public static boolean choseDialog(String inputDioalog, String title) {
-        return JOptionPane.showConfirmDialog(null,
-                inputDioalog, title, JOptionPane.YES_NO_OPTION) == 0;
+    @Override
+    public void pauseGame() {
+        status = GameStatusType.PAUSE;
+
     }
 
-    /**
-     * The entry point of application.
-     *
-     * @param args the input arguments
-     */
-    public static void main(String[] args) {
-
-
-        System.out.println("Start");
-
-        //                CALCOLO PATH RELATIVO UNIVERSALE
-        //----------------------------------------------------------------------
-        String tempPath = new File(
-                String.valueOf(BlackJack.class.getPackage()).replace("package ", "").replace(".", "/")
-        ).getParent();
-        File uesrPath = new File(System.getProperty("user.dir"));
-        String projectPath = uesrPath.getName().equals(tempPath) ?
-                uesrPath.getPath() :
-                new File(uesrPath.getPath() + "/src").exists() ?
-                        uesrPath.getPath() + "/src/" + tempPath :
-                        uesrPath.getPath() + tempPath;
-        //----------------------------------------------------------------------
-
-
-        // COSTANTI
-        String resursesPath = "/file/";
-        String rulesName = "rules.txt";
-
-        // Create a Logger with class name GFG
-        Logger logger = Logger.getLogger(BlackJack.class.getName());
-
-        confirmDialog("Benvenuto in BlackJack", "BlackJack");
-
-        if (!choseDialog("Conosci giá le regle?", "BlackJack")) {
-            // show REGOLE
-            try {
-                BufferedReader reader = new BufferedReader(new FileReader(projectPath + resursesPath + rulesName));
-                String currentLine = reader.readLine();
-                while (currentLine != null) {
-                    confirmDialog(currentLine, "RULES");
-                    currentLine = reader.readLine();
-                }
-
-                reader.close();
-            } catch (IOException e) {
-                logger.log(Level.WARNING, e.toString());
-            }
-        }
-
-        Dealer dealer = new Dealer();
-
-        Player player = new Player(100);
-
-
-        player.setPuntata(numberInput("inserire punata"));
-
-        System.out.println(player);
-
-        System.out.println("End");
-
+    @Override
+    public void endGame() {
+        status = GameStatusType.END;
     }
 }
