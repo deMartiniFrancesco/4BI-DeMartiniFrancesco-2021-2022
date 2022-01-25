8349013e5140f6cfb64825bc23d38916cd861460
deMartiniFrancesco <18606@studenti.marconiverona.edu.it>
2022-01-25 09:20:08 +0100

    git ignore

--- out/production/Git_INI/production/Java_modules/addPackageToProject.py
+++ out/production/Git_INI/production/Java_modules/addPackageToProject.py
@@ -5,39 +5,36 @@ __version__ = "0101 2021/11/30"
 from genericpath import isdir, isfile
 import os
 
-
 exist = False
 percorso, tail = os.path.split(__file__)
 os.chdir(percorso)
 
-
 boold = True
 if __name__ == '__main__':
-	if boold:
-		print("Start")
+    if boold:
+        print("Start")
 
-	projectListDir = os.listdir()
+    projectListDir = os.listdir()
 
-	for dir in projectListDir:
-		if isdir(dir) and dir.startswith("demartini"):
-			package = "package " + dir + ".bin;\n"
-			dir += "/bin/"
-			if isdir(dir):
-				for javaFilePath in os.listdir(dir):
-					javaFilePath = dir + javaFilePath
-					if isfile(javaFilePath) and javaFilePath.endswith(".java"):
-						
+    for directory in projectListDir:
+        if isdir(directory) and directory.startswith("demartini"):
+            package = "package " + directory + ".bin;\n"
+            directory += "/bin/"
+            if isdir(directory):
+                for javaFilePath in os.listdir(directory):
+                    javaFilePath = directory + javaFilePath
+                    if isfile(javaFilePath) and javaFilePath.endswith(".java"):
 
-						javaFile = open(javaFilePath, "r",)
-						listLines = javaFile.readlines()
-						javaFile.close()
+                        javaFile = open(javaFilePath, "r", )
+                        listLines = javaFile.readlines()
+                        javaFile.close()
 
-						if(package not in listLines):
-							print(javaFilePath)
-							listLines.insert(0, package + "\n")
-							javaFile = open(javaFilePath, "w",)
-							listLines = javaFile.writelines(listLines)
-							javaFile.close()
-	
-	if boold:
-		print("End")
+                        if package not in listLines:
+                            print(javaFilePath)
+                            listLines.insert(0, package + "\n")
+                            javaFile = open(javaFilePath, "w", )
+                            listLines = javaFile.writelines(listLines)
+                            javaFile.close()
+
+    if boold:
+        print("End")
